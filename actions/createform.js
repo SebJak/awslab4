1e4c4183042b19d3541db2a86f96858dbe7359da
Sebastian Jakowski <sebastian.jakowski@beneering.com>
2015-01-13 21:00:49 +0100

    Add loging to simpleDB

--- actions/createform.js
+++ actions/createform.js
@@ -6,9 +6,17 @@ var AWS_CONFIG_FILE = "config.json";
 var POLICY_FILE = "policy.json";
 var INDEX_TEMPLATE = "index.ejs";
 var ip = require('ip');
+var simpleDB = require("../simpleDB");
+
 
 
 var task = function(request, callback){
+	//LOg IP to simple DB
+	console.log("log ip to simple DB");
+	
+	simpleDB.putIpAddress();
+	//simpleDB.createDomain();
+	console.log("------------------");
 	//1. load configuration
 	var awsConfig = helpers.readJSONFile(AWS_CONFIG_FILE);
 	var policyData = helpers.readJSONFile(POLICY_FILE);
@@ -18,7 +26,7 @@ var task = function(request, callback){
 	
 
 	var bucektName = policy.getConditionValueByKey("bucket");
-	console.log(bucektName);
+	//console.log(bucektName);
 	
 	//3. generate form fields for S3 POST
 	var s3Form = new S3Form(policy);
@@ -26,9 +34,10 @@ var task = function(request, callback){
 	
 
 	var fields = s3Form.generateS3FormFields();
-	fields = s3Form.addS3CredientalsFields(fields, awsConfig);
 	//fields = policy.addConditions(fields, "x-amz-meta-addressIP", ip.address());
-	console.log("Fields: ",fields);
+	fields = s3Form.addS3CredientalsFields(fields, awsConfig);
+	
+	//console.log("Fields: ",fields);   {"x-amz-meta-addressIP": "123.123.212.0"}
 	//4. get bucket name
 	
 	callback(null, {template: INDEX_TEMPLATE, params:{fields: fields, bucket: bucektName}});
